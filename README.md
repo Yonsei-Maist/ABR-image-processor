83ab278138af609788a13c2e1b73acbcc54bb587
chanwoo kwon <arknell@yonsei.ac.kr>
2020-09-12 14:25:52 +0900

    read me updated

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # ABR-image-processor
 
 image proccess using opencv-python and yolo with tensorflow
+1. extract x, y values from graph image
+2. extract peak from image (if image have peak)
+
+## Environment
+```
+python 3.7 ~
+opencv-python lastest
+```
+
+## Install
+Need authorization
+```
+pip install -U git+https://git@github.com/Yonsei-Maist/ABR-image-processor.git
+```
+
+## Author
+Chanwoo Gwon, Yonsei Univ. Researcher since 2020.05~
+
+## Maintainer
+Chanwoo Gwon, arknell@yonsei.ac.kr (2020.09 ~)
--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from imagelib.extractor import Extractor
 import matplotlib.pyplot as plt
 
 extractor = Extractor()
-graph_list = extractor.extract_with_peak("/Users/gwonchan-u/Downloads/Infant ABR 51-110/test2.png", True)
+graph_list = extractor.extract_with_peak("/Users/gwonchan-u/Downloads/Infant ABR 51-110/test9.png", True)
 
 # print(graph_list)
 
 for graph_info in graph_list:
     graph = graph_info["graph"]
-    peak = graph_info["peak"]
+    peak_list = graph_info["peak"]
     plt.plot(range(len(graph)), graph, linewidth=1)
-    if peak != (-1, -1):
+    for peak in peak_list:
         plt.scatter(peak[0], peak[1])
 
 # print(graph_peak)
